8994eef025fc6ba01c94408adb4bdc3c842e0145
prafful <prraful@gmail.com>
2018-01-17 17:48:52 +0530

    custom directive and JSON

--- angularcode.js
+++ angularcode.js
@@ -6,6 +6,37 @@ function implementsController($scope){
     $scope.location = "Chennai";
     $scope.counter = 1;
     $scope.friends = ['Ida', 'Anita', 'nazeem','Mani'];    
+    $scope.friends_json = [
+                            
+                            {
+                                name:'Ida',
+                                score:"25",
+                                dob:"31/08/85",
+                                earning:500
+                                },   
+                                {
+                                    name:'Mani',
+                                    score:"25",
+                                    dob:"25/08/85",
+                                    earning:450
+                                    },   
+                                    {
+                                        name:'Ram',
+                                        score:"50",
+                                        dob:"02/08/85",
+                                        earning:300
+                                        },   
+                                        {
+                                            name:'Chenna',
+                                            score:"85",
+                                            dob:"31/01/85",
+                                            earning:900
+                                            },     
+                             
+                        ];   
+
+    $scope.date_sample = new Date();
+    $scope.show_score = true;
     $scope.updateMe = function(){
         console.log($scope.myname);
     }
@@ -21,6 +52,32 @@ function implementsController($scope){
         $scope.friends.splice(received,1);
     }
 }
+
+/* 
+app.directive("w3TestDirective", function() {
+    return {
+        restrict : "A",
+        template : "<h1>Made by a directive!</h1>"
+    };
+});
+ */
+
+ app.directive("moneyUl", implementDirective1);
+
+ function implementDirective1(){
+    return {
+        restrict:"E",
+        template:`<ul>
+        <li>Hello</li>
+        <li>World</li>
+        </ul>`
+    };
+ }
+
+
+
+
+
 
 
 
