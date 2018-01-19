1bd7975334177256edaa5c59b6c28c8f7f38b12f
prafful <prraful@gmail.com>
2018-01-19 17:04:03 +0530

    angulrjs http example

--- angular_service_factory.js
+++ angular_service_factory.js
@@ -1,17 +1,8 @@
 var app = angular.module("sad", []);
-app.controller("happy", additionController);
 
-function additionController($scope, additionService){
-    $scope.addNumberForm = function(){
-        $scope.totalAdd = additionService.addNumber($scope.number1, $scope.number2);
-    }
-    $scope.multiplyNumberForm = function(){
-        $scope.totalMultiply = additionService.multiplyNumber($scope.number1, $scope.number2);
-    }
-}
 
 app.service("additionService", additionServiceFunction);
-function additionServiceFunction(){
+function additionServiceFunction($http){
     var addition = {};
     addition.addNumber = function(number1, number2){
         return number1 + number2;
@@ -19,6 +10,42 @@ function additionServiceFunction(){
 
     addition.multiplyNumber =function(number1, number2){
         return number1 * number2;
-    } 
+    }
+    
+    addition.getRemoteData = function(){
+        console.log("Inside get remote data")
+        $http({
+            method:"GET", 
+            url:"https://jsonplaceholder.typicode.com/users"
+        }).then(onSuccess, onFailure);
+        function onSuccess(response){
+            console.log("success..." + response.data);
+            return response.data;
+        }
+        function onFailure(error){
+            console.log("failure..." + error.statusText);
+            return error.statusText;
+        }
+    }
+    console.log("returning..." + addition);
     return addition;
+}
+
+
+app.controller("happy", additionController);
+
+function additionController($scope, additionService, $http){
+    $scope.remoteData = {};
+
+    $scope.addNumberForm = function(){
+        $scope.totalAdd = additionService.addNumber($scope.number1, $scope.number2);
+    }
+    $scope.multiplyNumberForm = function(){
+        $scope.totalMultiply = additionService.multiplyNumber($scope.number1, $scope.number2);
+    }
+
+    $scope.getRemoteData = function(){
+        $scope.remoteData = JSON.stringify(additionService.getRemoteData());
+        console.log("in controller..." + $scope.remoteData);
+    }
 }
